b8daa899ba1e8a8ebf1303059f48b5d201ae93d1
shouta.hirakawa <hirakawa@brainnet.co.jp>
2022-03-27 09:58:09 +0000

    bug fixed_2

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -15,7 +15,7 @@
                         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
                         <ul class="dropdown-menu dropdown-menu-right">
                             {{-- 新規タスクの投稿へのリンク --}}
-                            <li class="dropdown-item">{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li>
+                            <li class="dropdown-item">{!! link_to_route('tasks.create', 'new task', ['user' => Auth::id()]) !!}</li>
                             <li class="dropdown-divider"></li>
                             {{-- ログアウトへのリンク --}}
                             <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
